44c954e6ce7b0e7db70c62b3cec1d0fa3a2113ab
saidgc <said.g@hotmail.com>
2019-05-28 19:59:08 -0500

    Twitter stream

---
+++ stream.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import json
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import apikeys
+
+
+class StdOutListener(StreamListener):
+    def on_data(self, data):
+        DataParser().convert(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+
+class DataParser:
+    def convert(self, data):
+        jso = json.loads(data)
+        if "entities" in jso:
+            if "media" in jso['entities']:
+                pass
+        try:
+            user = jso['user']['screen_name']
+            text = jso['text']
+            date = jso['created_at']
+            print(text)
+        except Exception as e:
+            print("Error 123", e)
+
+
+if __name__ == '__main__':
+    listener = StdOutListener()
+    auth = OAuthHandler(apikeys.consumer_key, apikeys.consumer_secret)
+    auth.set_access_token(apikeys.access_token, apikeys.access_token_secret)
+    stream = Stream(auth, listener)
+
+    # stream.filter(track=['#TenemosSismo', '#sasmex', '#TenemosAlerta', '#AlertaSísmica'])
+    stream.filter(track=['#DiaDeLaHamburguesa'])
