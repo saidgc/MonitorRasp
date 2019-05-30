f5d5dc4be64bc290e32869606ed95b23eacab28e
saidgc <said.g@hotmail.com>
2019-05-29 20:37:29 -0500

    Apikeys

--- apikeys.py
+++ apikeys.py
@@ -1,6 +1,6 @@
 # Twitter API keys
 
-access_token = ""
-access_token_secret = ""
-consumer_key = ""
-consumer_secret = ""
+access_token = "Twitter access_token"
+access_token_secret = "Twitter access_token_secret"
+consumer_key = "Twitter consumer_key"
+consumer_secret = "Twitter consumer_secret"
